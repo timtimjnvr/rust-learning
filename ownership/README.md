5562c702b05e2c49b14906c92438c2ed7653caf1
JANVIER Timothee <Timothee.Janvier@bordeaux-inp.fr>
2023-12-11 08:32:49 +0100

    Referenes and borowing

--- ownership/README.md
+++ ownership/README.md
@@ -1,3 +1,5 @@
+# Heap & Stack
+
 - stack & heap are both part of the memory.
 - stack : stores and removes values *in LIFO mode*. Adding data is called *pushing onto the stack* removing is called *popping off 
 the stack*. data must have *fixed size*.
@@ -9,3 +11,11 @@ which is the adress to the location.
 - when you call a function, the values passed to that function and the function local variables get pushed on to the stack. When 
 the function is over those values get popped of the stack.
 - main objective of ownership : **to manage heap data**.
+
+# Referenes and borowing
+
+- reference is like a pointer : an adress we can follow to get data from memory, unlike a pointer a reference garantee to point to a valid value of a particular type for the life of that reference.
+- reference allows to refer to some value without taking ownership of it.
+- values passed by reference are not borrowed so they are not droped when the reference stops being used.
+- values passed to function by reference can't be modifiable, you need to use mutable references.
+- a mutable variable can only have one mutable reference : this prevents data race at compile time.
--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -67,6 +67,36 @@ fn main() {
     let s1 = String::from("hello");
     let (s2, len) = calculate_length(s1);
     println!("The length of '{}' is {}.", s2, len);
+
+    let len2 = calculate_length_with_pointer(&s2);
+    println!("The length of '{}' is {}.", s2, len2);
+
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("{s}");
+
+    let mut s = String::from("hello");
+    {
+        let r1 = &mut s;
+        println!("{r1}");
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+    let _r2 = &mut s;
+
+    let mut s = String::from("hello");
+    println!("{s}");
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+                 // let r3 = &mut s; // BIG PROBLEM because reference of s is used by r1 & r2
+    println!("{}{}", r1, r2);
+    let r3 = &s; // no problem
+    let r4 = &s; // no problem
+
+    println!("{} and {}", r3, r4);
+    // variables r1 and r2 will not be used after this point (popped of the stack)
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
 }
 
 fn takes_ownership(some_string: String) {
@@ -104,4 +134,14 @@ fn calculate_length(s: String) -> (String, usize) {
     let length = s.len(); // len() returns the length of a String
 
     (s, length)
+}
+
+fn calculate_length_with_pointer(s: &String) -> usize {
+    // s is a reference to a String
+    s.len()
+} // Here, s goes out of scope. But because it does not have ownership of what
+  // it refers to, it is not dropped.
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
