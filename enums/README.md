7156541d6325480f5b87f2fa9f34e3eee04a7ca8
JANVIER Timothee <Timothee.Janvier@bordeaux-inp.fr>
2023-12-18 08:40:00 +0100

    Match & let if

--- enums/README.md
+++ enums/README.md
@@ -1,11 +1,22 @@
+# Enums
 - enums allows to group types and attach data to variants.
 - each variant can have its own data.
+
 # Option instead of null
-- rust does not have ```null``` values. It uses ```Option``` instead (which is an enum, calleable in any scope without using ```Option::None``` just ```None``````)
+- rust does not have ```null``` values. It uses ```Option``` instead (which is an enum, callable in any scope without using ```Option::None``` just ```None```)
 ```
 // generic enum (can hold any type)
 enum Option<T> {
     None,
     Some(T),
-}```
-- Why Option instead of null : because Option<T> and T (where T can be any type) are different types, the compiler won’t let us use an Option<T> if the value is not a valid value specified in the enum definition.
+}
+```
+
+- Why ```Option``` instead of ```null``` : because ```Option<T>``` and ```T``` (where ```T``` can be any type) are different types, the compiler won’t let us use an ```Option<T>``` if the value is not a valid value specified in the enum definition.
+- This prevents the [billion-dollar mistake](https://www.infoq.com/presentations/Null-References-The-Billion-Dollar-Mistake-Tony-Hoare/) !
+
+# Match & let if
+- match list need to be exaustive !
+- ```other``` matches all non enumerated preceeding cases and bind the value in the other variable.
+- ```_```  matches all non enumerated preceeding cases but does not bind any value.
+- ```let if``` can be useful when having one or two cases (more consise).
--- enums/src/main.rs
+++ enums/src/main.rs
@@ -26,6 +26,19 @@ impl Message {
     }
 }
 
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
 fn main() {
     // Create instance of each of the two variants
     // the variants are namespaced under its identifier
@@ -50,6 +63,60 @@ fn main() {
     let x: i8 = 5;
     let y: Option<i8> = Some(5);
     // let sum = x + y; will panic you need to verify the Option is not a None variant using match !
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        // this match all other possible cases !
+        // need to be put at last because patterns are evaluated in order.
+        other => move_player(other),
+        // this pattern evaluates to all other types but does not bind the value.
+        _ => reroll(),
+    }
+
+    fn add_fancy_hat() {}
+    fn remove_fancy_hat() {}
+    fn move_player(num_spaces: u8) {}
+    fn reroll() {}
+
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
 }
 
 fn route(ip_kind: IpAddrKind) {}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    // arms MUST cover all possibilities ! prevents from unexpected scenarios.
+    // if a match is missing a variant, it won't compile.
+    match coin {
+        // evaluates in order
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
