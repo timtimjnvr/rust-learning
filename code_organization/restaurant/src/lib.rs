17e23f92657f7cca35fe5ec26909269a9c1490c4
JANVIER Timothee <Timothee.Janvier@bordeaux-inp.fr>
2023-12-19 08:31:25 +0100

    Paths and privacy

--- code_organization/restaurant/src/lib.rs
+++ code_organization/restaurant/src/lib.rs
@@ -6,8 +6,9 @@ mod front_of_house {
     // inner modules
     // hosting and serving are sibling modules
     // hosting and serving are child modules of front_house
-    mod hosting {
-        fn add_to_waitlist() {}
+    // we make module public for it to be callable from parents modules.
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -19,4 +20,59 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+// part of our library public API
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    //  won't compile
+    // seasonal_fruit is private !
+    // meal.seasonal_fruit = String::from("blueberries");
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        // private field
+        seasonal_fruit: String,
+    }
+
+    // all the variants of a publid enum are public !
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    impl Breakfast {
+        // public constructor
+        // needed to instantiate the struct Breakfast (we can't set the private field from the parent module directly)
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        // parent (relative path)
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
 }
