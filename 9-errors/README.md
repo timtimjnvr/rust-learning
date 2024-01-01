49bd07c173656d801c01693142a5afbbf75993d7
timtimjnvr <timotheejanvier01@gmail.com>
2024-01-01 16:36:49 +0100

    Errors handling - to panic or not panic

--- 9-errors/README.md
+++ 9-errors/README.md
@@ -7,3 +7,15 @@ panic = 'abort'
 
 - to get backtrace of a call that panics, use `RUST_BACKTRACE=1 cargo run`. Debug symbols need to be enabled to do this (enabled by default). They are not enables when running in release mode (`--release` flag)
 - the buffer overread is not possible in rust (unlike C).
+
+- the `?` operator : allows to return from a function with the given return type if the `Result` of an expression is an `Err`. It can only be used in functions which return type is compatible with value the `?` is used on (`Result` or `Option`).
+- to convert a `Result` into an `Option` and vice versa, you need to use `ok` method on `Result` and reciprocally the method `ok_or` on `Option`.
+
+- `Box<dyn Error>`` means “any kind of error in main function”.
+- when the main function returns a `Result<T,E>`, the executable will return 0 if `Ok` and a non zero value if main returns an `Err` (same as in C).
+- the main function can return any type that has the adequate function (termination trait) which returns an exit code.
+
+# how
+
+- when you know a function call can't fail it's acceptable to call `expect`.
+- it's ok to panic if your code is in a bad state (something unexpected as opposed to something that could likely happen)
--- 9-errors/src/main.rs
+++ 9-errors/src/main.rs
@@ -1,7 +1,11 @@
+use std::error::Error;
+use std::fs;
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::{self, ErrorKind, Read};
 
-fn main() {
+// Box<dyn Error>> means “any kind of error.”
+// when the main function returns a Result<T,E>, the executable will return 0 if Ok and a non zero value if main returns an Err (same as C)
+fn main() -> Result<(), Box<dyn Error>> {
     // panic!("crash and burn");
 
     // let v = vec![1, 2, 3];
@@ -9,6 +13,7 @@ fn main() {
     // in Rust, it will stop execution and refuse to continue (no security vulnerability)
     // v[99];
 
+    /*
     let _greeting_file_result = match File::open("hello.txt") {
         Ok(file) => file,
         Err(error) => match error.kind() {
@@ -21,10 +26,12 @@ fn main() {
             }
         },
     };
+     */
 
     // cleaner way to do this with closures
     // unwrap_or_else returns the value inside Ok or calls the closure code in case of Err
-    let _greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+    /*
+       let _greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
         if error.kind() == ErrorKind::NotFound {
             File::create("hello.txt").unwrap_or_else(|error| {
                 panic!("Problem creating the file: {:?}", error);
@@ -33,12 +40,59 @@ fn main() {
             panic!("Problem opening the file: {:?}", error);
         }
     });
+     */
 
     // unwrap directly calls panic on a result.
     // let _greeting_file = File::open("other_file.txt").unwrap();
 
     // adding expect allows to add an error message and precise the intent in case of error
     // it unwraps the result if Ok
-    let _greeting_file =
-        File::open("other_file.txt").expect("other_file.txt should be included in the project");
+    // let _greeting_file = File::open("other_file.txt").expect("other_file.txt should be included in the project");
+
+    let _greeting_file = File::open("hello.txt")?;
+
+    Ok(())
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file_v2() -> Result<String, io::Error> {
+    // ? operator allow to return the Err from the whole function or to continue if Ok
+    // the ? operator returns the Err as an io::Error (converted)
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_v3() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
+
+fn read_username_from_file_much_simpler() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    // next return the next line as a string (here it returns None if the line is empty)
+    // the ? allows to returns in case of None
+    text.lines().next()?.chars().last()
 }
