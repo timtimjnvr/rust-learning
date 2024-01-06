c89ea0c868a39ad017f183a49cbf98d0c7a92e56
timtimjnvr <timotheejanvier01@gmail.com>
2024-01-06 12:05:24 +0100

    Search text in file & test

--- 12-minigrep/src/lib.rs
+++ 12-minigrep/src/lib.rs
@@ -25,6 +25,38 @@ impl Config {
 // dyn is a short for dynamic
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{contents}");
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
     Ok(())
+}
+
+// lifetime indicates that the returned vec of string slice references refers to contents rather than query
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
