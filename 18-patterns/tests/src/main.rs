715d804b927dbf320853f2a8602791327354d5aa
timtimjnvr <timotheejanvier01@gmail.com>
2024-01-16 19:18:25 +0100

    Patterns with @

--- 18-patterns/tests/src/main.rs
+++ 18-patterns/tests/src/main.rs
@@ -119,6 +119,23 @@ fn main() {
     }
 
     println!("at the end: x = {:?}, y = {y}", x);
+
+    enum Message {
+        Hello { id: i32 },
+    }
+
+    let msg = Message::Hello { id: 5 };
+
+    match msg {
+        Message::Hello {
+            // if value matches the range, the variable is captured and the arm executed
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {}", id_variable),
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => println!("Found some other id: {}", id),
+    }
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
